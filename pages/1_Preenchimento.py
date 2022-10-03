8c0eedf5c11953452c75be5ec51ca656f97f3436
Renan Carneiro <renan.moraes@engenharia.ufjf.br>
2022-10-03 13:37:07 -0300

    v1.2.2
    Aumentei o tamanho do formulário de preenchimento
    capitalizei o titulo da pagina home->Home

--- pages/1_Preenchimento.py
+++ pages/1_Preenchimento.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.write("Insira abaixo os dados.")
+st.write("<center>Insira abaixo os dados.</center>", unsafe_allow_html=True )
 formulario="""
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeBZoiGYHM5PL6rxBkHfX6AuFzFdccv1F9iruUpeMaHGYLDFA/viewform?embedded=true" width="700" height="520" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeBZoiGYHM5PL6rxBkHfX6AuFzFdccv1F9iruUpeMaHGYLDFA/viewform?embedded=true" width="100%" height="820" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
         """
 
 st.markdown(formulario, unsafe_allow_html=True)
