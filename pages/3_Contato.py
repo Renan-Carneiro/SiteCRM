d9b466d002c56070e361b89ca5a98f4d96627aa7
Renan Carneiro <113647888+Renan-Carneiro@users.noreply.github.com>
2024-01-07 16:13:12 -0300

    Update 3_Contato.py
    Tentei estilizar melhor a pagina

--- pages/3_Contato.py
+++ pages/3_Contato.py
@@ -7,21 +7,52 @@ st.write('32 999189393')
 st.write('renan.carneiro@okuscapital.com')
 
 with st.container():
-        st.write("---")
-        st.header("Me envie um email!")
-        st.write('##')
+    st.write("---")
+    st.header("Me envie um email!")
+    st.write('##')
 
-        contact_form = """
+    contact_form = """
+    <style>
+        .contact-form {
+            max-width: 400px;
+            margin: auto;
+            padding: 20px;
+            background-color: #f4f4f4;
+            border-radius: 10px;
+        }
+        .form-input {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+        .form-textarea {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+        .form-button {
+            width: 100%;
+            padding: 10px;
+            background-color: #007BFF;
+            color: #fff;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+    </style>
+
+    <div class="contact-form">
         <form action="https://formsubmit.co/renan.carneiro@okuscapital.com" method="POST">
-            <input type="hidden" name="_captcha" value="false">
-            <input type="text" name="name" placeholder="Seu nome" required>
-            <input type="email" name="email" placeholder="Seu email" required>
-            <textarea name="message" placeholder="Sua mensagem" required></textarea>
-            <button type="submit">Enviar</button>
+            <input class="form-input" type="hidden" name="_captcha" value="false">
+            <input class="form-input" type="text" name="name" placeholder="Seu nome" required>
+            <input class="form-input" type="email" name="email" placeholder="Seu email" required>
+            <textarea class="form-textarea" name="message" placeholder="Sua mensagem" required></textarea>
+            <button class="form-button" type="submit">Enviar</button>
         </form>
-        """
-        left_column,right_column = st.columns(2)
-        with left_column:
-            st.markdown(contact_form, unsafe_allow_html=True)
-        with right_column:
-            st.empty()
+    </div>
+    """
+    st.markdown(contact_form, unsafe_allow_html=True)
